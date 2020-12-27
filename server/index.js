7c220c3b00be523ef2adfc1b90ed44d83997a359
Guille <gfgorostidi@protonmail.com>
2020-12-27 11:19:43 +0100

    Add /api/trucos endpoint to server

--- server/index.js
+++ server/index.js
@@ -1,6 +1,6 @@
-const express = require('express');
-const bodyParser = require('body-parser');
-const cors = require('cors');
+const express = require("express");
+const bodyParser = require("body-parser");
+const cors = require("cors");
 
 const app = express();
 
@@ -9,17 +9,19 @@ const app = express();
 app.use(bodyParser.json());
 app.use(cors());
 
-const posts = require('./routes/api/posts');
+const posts = require("./routes/api/posts");
+const trucos = require("./routes/api/trucos");
 
-app.use('/api/posts', posts);
+app.use("/api/posts", posts);
+app.use("/api/trucos", trucos);
 
 // Handle production
-if(process.env.NODE_ENV === 'production') {
-    // Static folder
-    app.use(express.static(__dirname + '/public/'));
+if (process.env.NODE_ENV === "production") {
+  // Static folder
+  app.use(express.static(__dirname + "/public/"));
 
-    // Handle SPA
-    app.get(/.*/, (req, res) => res.sendFile(__dirname + '/public/index.html'));
+  // Handle SPA
+  app.get(/.*/, (req, res) => res.sendFile(__dirname + "/public/index.html"));
 }
 
 const port = process.env.PORT || 5000;
---
+++ server/routes/api/trucos.js
@@ -0,0 +1,52 @@
+const express = require("express");
+const mongodb = require("mongodb");
+
+const router = express.Router();
+
+// Get trucos
+router.get("/", async (req, res) => {
+  const trucos = await loadTrucosCollection();
+  res.send(await trucos.find({}).toArray());
+});
+
+// Add truco
+router.post("/", async (req, res) => {
+  if (req.body) {
+    const trucos = await loadPostsCollection();
+    await trucos.insertOne({
+      text: req.body.text,
+      creationDate: new Date()
+    });
+    res.status(201).send();
+
+    console.log("------------------");
+    console.log(`New trucos created: ${req.body.text}`);
+  } else {
+    res.status(400).end();
+  }
+});
+
+// Delete truco
+router.delete("/:id", async (req, res) => {
+  if (req.params && req.params.id) {
+    const trucos = await loadPostsCollection();
+    await trucos.deleteOne({ _id: new mongodb.ObjectID(req.params.id) });
+    res.status(200).send();
+  } else {
+    res.status(400).end();
+  }
+});
+
+async function loadTrucosCollection() {
+  const client = await mongodb.MongoClient.connect(
+    "mongodb+srv://abc123:7GoHm9p412Hx0eDN@cluster0-t3pah.mongodb.net/test?retryWrites=true&w=majority",
+    {
+      useNewUrlParser: true,
+      useUnifiedTopology: true
+    }
+  );
+
+  return client.db("fullstack_vue_express").collection("trucos");
+}
+
+module.exports = router;
