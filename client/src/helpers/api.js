4de01d4857b6849df7713bbc95cfee8b4586b548
Guille <gfgorostidi@protonmail.com>
2021-04-04 13:52:39 +0200

    Migrate to Vue3

--- client/src/helpers/api.js
+++ client/src/helpers/api.js
@@ -1,12 +1,6 @@
 import axios from "axios";
 
 export default {
-  fetchPeople(page) {
-    return axios
-      .get(`${apiUrl}/people/?page=${page}`)
-      .then(res => res.data)
-      .catch(error => error);
-  },
   callURL(url) {
     return axios
       .get(url)
